b532354863cd09315292c8c9530c6780763d3137
Yeon Kang <47591097+lovelykite@users.noreply.github.com>
2021-11-19 11:19:42 +0900

    Add files via upload

--- vision/hue_detection.py
+++ vision/hue_detection.py
@@ -5,54 +5,35 @@ import rospy
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from cv_bridge.boost.cv_bridge_boost import getCvType
 
-class hue_detect:
 
-    def __init__(self):
-        self.image_pub = rospy.Publisher("/COSRA/hue_detection",Image)
+usb_cam = cv2.VideoCapture(0)
 
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
-
-    def callback(self,data):        
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            print(e)
-
-        (rows,cols,channels) = cv_image.shape
-        if cols > 60 and rows > 60 :
-            cv2.circle(cv_image, (50,50), 10, 255)
-
-        # Reading the video from the
-        # webcam in image frames 
-        _, imageFrame = cv_image.read() 
+def hue_detect():
+    image_pub = rospy.Publisher("/COSRA/hue_detection",Image, queue_size=10)
+    rospy.init_node('hue_detection', anonymous=True)
+    #rate = rospy.Rate(30) # 30hz
+    bridge = CvBridge()
+    while not rospy.is_shutdown():
         
-        # Convert the imageFrame in 
-        # BGR(RGB color space) to 
-        # HSV(hue-saturation-value) 
-        # color space 
+        _, imageFrame = usb_cam.read() 
+        
         hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV) 
         
-        # Set range for red color and 
-        # define mask 
         red_lower = np.array([136, 87, 111], np.uint8) 
         red_upper = np.array([180, 255, 255], np.uint8) 
         red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
-        # Set range for green color and 
-        # define mask 
+        
         green_lower = np.array([25, 52, 72], np.uint8) 
         green_upper = np.array([102, 255, 255], np.uint8) 
         green_mask = cv2.inRange(hsvFrame, green_lower, green_upper) 
-        # Set range for blue color and 
-        # define mask 
+        
         blue_lower = np.array([94, 80, 2], np.uint8) 
         blue_upper = np.array([120, 255, 255], np.uint8) 
         blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper) 
-        # Morphological Transform, Dilation 
-        # for each color and bitwise_and operator 
-        # between imageFrame and mask determines 
-        # to detect only that particular color kernal = np.ones((5, 5), "uint8") 
+       
+        kernal = np.ones((5, 5), "uint8") 
 
         # For red color 
         red_mask = cv2.dilate(red_mask, kernal) 
@@ -65,52 +46,42 @@ class hue_detect:
         res_blue = cv2.bitwise_and(imageFrame, imageFrame, mask = blue_mask) 
         
         # Creating contour to track red color 
-        contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+        _, contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
         for pic, contour in enumerate(contours): 
             area = cv2.contourArea(contour) 
             if(area > 300): 
                 x, y, w, h = cv2.boundingRect(contour) 
                 imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 0, 255), 2) 
-                cv2.putText(imageFrame, "Red Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255)) 
+                cv2.putText(imageFrame, "Red", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255)) 
                 
         # Creating contour to track green color 
-        contours, hierarchy = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+        _, contours, hierarchy = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
         for pic, contour in enumerate(contours): 
             area = cv2.contourArea(contour) 
             if(area > 300): 
                 x, y, w, h = cv2.boundingRect(contour) 
                 imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 255, 0), 2) 
-                cv2.putText(imageFrame, "Green Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0)) 
+                cv2.putText(imageFrame, "Green", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0)) 
                     
         # Creating contour to track blue color 
-        contours, hierarchy = cv2.findContours(blue_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+        _, contours, hierarchy = cv2.findContours(blue_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
         for pic, contour in enumerate(contours): 
             area = cv2.contourArea(contour) 
             if(area > 300): 
                 x, y, w, h = cv2.boundingRect(contour) 
                 imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (255, 0, 0), 2) 
-                cv2.putText(imageFrame, "Blue Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0)) 
+                cv2.putText(imageFrame, "Blue", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0)) 
 
         cv2.imshow("Image window", imageFrame)
         cv2.waitKey(3)
-
         try:
-            self.image_pub.publish(self.bridge.cv2_to_imgmsg(imageFrame, "bgr8"))
+            image_pub.publish(bridge.cv2_to_imgmsg(imageFrame, "bgr8"))
         except CvBridgeError as e:
             print(e)
-
-
-        #webcam = cv2.VideoCapture(0)
-        
-        
-def main(args):
-  ic = hue_detect()
-  rospy.init_node('hue_detection', anonymous=True)
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
+        # rate.sleep()
 
 if __name__ == '__main__':
-    main(sys.argv)
+    try:
+        hue_detect()
+    except rospy.ROSInterruptException:
+        pass
