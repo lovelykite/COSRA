2f326bd1b02f8f422c9375ffbc2539fb0fdb27fa
Yeon Kang <47591097+lovelykite@users.noreply.github.com>
2021-12-16 22:58:16 +0900

    Add files via upload

--- vision/sorting_box_RGB_ROS.py
+++ vision/sorting_box_RGB_ROS.py
@@ -44,16 +44,16 @@ def hue_detect(sub_img, i):
 
     #####################For the Test##########################
     
-    red_lower = np.array([124, 66, 136], np.uint8) 
-    red_upper = np.array([179, 178, 255], np.uint8) 
+    red_lower = np.array([0, 117, 104], np.uint8) 
+    red_upper = np.array([2, 255, 176], np.uint8) 
     red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
     
-    green_lower = np.array([83, 95, 109], np.uint8) 
-    green_upper = np.array([88, 246, 255], np.uint8) 
+    green_lower = np.array([60, 108, 67], np.uint8) 
+    green_upper = np.array([84, 255, 161], np.uint8) 
     green_mask = cv2.inRange(hsvFrame, green_lower, green_upper) 
     
-    blue_lower = np.array([95, 225, 181], np.uint8) 
-    blue_upper = np.array([103, 255, 211], np.uint8) 
+    blue_lower = np.array([96, 204, 120], np.uint8) 
+    blue_upper = np.array([102, 255, 211], np.uint8) 
     blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper) 
 
     #############################################################
@@ -105,11 +105,13 @@ def sorting_box(color_goal, frame):
                     cv2.putText(frame, 'Move RED', (310, 30), font, 1, (0, 0, 255), 3)
                     cv2.imshow('frame', frame)
                     print("Move RED")
+                    start = color_goal[1, wrong[0][0]]
                 else:
                     wrong = np.where((color_goal[0, :] != 0) & (color_goal[0, :] != 1))
                     cv2.putText(frame, 'Move Other Color', (310, 30), font, 1, (0, 0, 0), 3)
                     cv2.imshow('frame', frame)
                     print("Move Other Color")
+                    start = color_goal[1, np.random.choice(wrong[0][:], 1)]
             elif empty[0][0] == 2 or empty[0][0] == 3:
                 if (color_goal[0, 0:2] == 2).any() == True or (color_goal[0, 4:] == 2).any() == True:
                     temp[2:4] = 0
@@ -117,11 +119,13 @@ def sorting_box(color_goal, frame):
                     cv2.putText(frame, 'Move GREEN', (310, 30), font, 1, (0, 255, 0), 3)
                     cv2.imshow('frame', frame)
                     print("Move GREEN")
+                    start = color_goal[1, wrong[0][0]]
                 else:
                     wrong = np.where((color_goal[0, :] != 0) & (color_goal[0, :] != 2))
                     cv2.putText(frame, 'Move Other Color', (310, 30), font, 1, (0, 0, 0), 3)
                     cv2.imshow('frame', frame)
                     print("Move Other Color")
+                    start = color_goal[1, np.random.choice(wrong[0][:], 1)]
             else:
                 if (color_goal[0, :4] == 3).any() == True:
                     temp[4:] = 0
@@ -129,13 +133,14 @@ def sorting_box(color_goal, frame):
                     cv2.putText(frame, 'Move BLUE', (310, 30), font, 1, (255, 0, 0), 3)
                     cv2.imshow('frame', frame)
                     print("Move BLUE")
+                    start = color_goal[1, wrong[0][0]]
                 else:
                     wrong = np.where((color_goal[0, :] != 0) & (color_goal[0, :] != 3))
                     cv2.putText(frame, 'Move Other Color', (310, 30), font, 1, (0, 0, 0), 3)
                     cv2.imshow('frame', frame)
                     print("Move Other Color") 
+                    start = color_goal[1, np.random.choice(wrong[0][:], 1)]
             
-            start = color_goal[1, wrong[0][0]]
             goal = color_goal[1, empty[0][0]]
 
             print("start :", start)
@@ -151,6 +156,9 @@ def sorting_box(color_goal, frame):
         cv2.putText(frame, 'Complete!', (310, 30), font, 1, (0, 0, 0), 3)
         cv2.imshow('frame', frame)
         print ("Sorting Box Complete!")
+        # start = None
+        # goal = None
+        # return start, goal
         
 # find parameters with Trackbar
 # cv2.namedWindow('parameters')
@@ -159,12 +167,12 @@ def sorting_box(color_goal, frame):
 # cv2.createTrackbar('Min pixels', 'parameters', 100, 1500, callback)
 # cv2.createTrackbar('Max pixels', 'parameters', 323, 1500, callback)
 
-rois = np.array([[205,125,80,80],
-        [205,220,80,80],
-        [301,125,80,80],
-        [301,220,80,80],
-        [398,125,80,80],
-        [398,220,80,80]])
+rois = np.array([[382,189,85,85],
+        [380,17,80,80],
+        [269,195,85,85],
+        [272,14,80,80],
+        [155,191,85,85],
+        [161,14,80,80]])
 # rois = [[int(float(j)) for j in i] for i in rois]
 color_goal = np.vstack((np.zeros(6),np.array([1, 2, 3, 4, 5, 6])))
 
@@ -223,7 +231,7 @@ if __name__ == '__main__':
         # max = cv2.getTrackbarPos('Max pixels', 'parameters')
         # lowThreshold = cv2.getTrackbarPos('Threshold1', 'parameters')
         # highThreshold = cv2.getTrackbarPos('Threshold2', 'parameters')
-        min = 700
+        min = 600
         max = 1500
         lowThreshold = 186
         highThreshold = 122
@@ -233,10 +241,15 @@ if __name__ == '__main__':
 
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(frame, 'Available spots: ' + str(spots.loc), (10, 30), font, 1, (0, 235, 255), 2)
-        cv2.imshow('frame', frame)
+        cv2.namedWindow('color', cv2.WINDOW_NORMAL)
+        cv2.imshow('color', frame)
+        cv2.resizeWindow('color', 1000, 800)  
 
         canny = cv2.Canny(frame2, lowThreshold, highThreshold)
+        cv2.namedWindow('canny', cv2.WINDOW_NORMAL)
         cv2.imshow('canny', canny)
+        cv2.resizeWindow('canny', 1000, 800)  
+        
 
         # print (color_goal)
         if flag == 0:
